1eb00d1a57de0484553a133d5ca87ddac3098e49
Kaung Zaw Htet <kaungzawhtet.mm@gmail.com>
2019-10-10 03:27:59 +0630

    First test codes

--- test/number_test.cpp
+++ test/number_test.cpp
@@ -2,3 +2,18 @@
 // Created by Kaung Zaw Htet on 2019-10-08.
 //
 
+#include <gtest/gtest.h>
+#include <iostream>
+#include "../lib/ShinMwayLoon/include/ph_number.h"
+
+
+TEST(NumberTest,NumberTest1){
+    std::cerr<<"This is number test 1"<<std::endl;
+    EXPECT_TRUE(true);
+
+}
+
+TEST(NumberTest,NumberTest2){
+    EXPECT_TRUE(false);
+    std::cerr<<"This is number test 2"<<std::endl;
+}
--- test/ph_num_test.cpp
+++ test/ph_num_test.cpp
@@ -2,3 +2,17 @@
 // Created by Kaung Zaw Htet on 2019-10-08.
 //
 
+#include <gtest/gtest.h>
+#include <iostream>
+
+
+TEST(PhoneNumberTest,NumberTest1){
+    std::cerr<<"This is Phone Number Text 1"<<std::endl;
+    EXPECT_TRUE(true);
+
+}
+
+TEST(PhoneNumberTest,NumberTest2){
+    EXPECT_TRUE(false);
+    std::cerr<<"This is Phone Number Text 2"<<std::endl;
+}
